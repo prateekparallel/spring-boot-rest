f618525439ef953e247a241b578dd3a571748ea7
prateekparallel <32555931+prateekparallel@users.noreply.github.com>
2018-07-24 23:14:47 +0100

    Update StudentRegisterController.java

--- src/main/java/com/bhaiti/rest/controller/StudentRegisterController.java
+++ src/main/java/com/bhaiti/rest/controller/StudentRegisterController.java
@@ -6,7 +6,7 @@ import com.bhaiti.beans.*;
 
 /**
  * @author prate
- *
+ * This is a controller class for registering a student
  */
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
